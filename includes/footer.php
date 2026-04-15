8b8b0f82c37bee35bf23274fe5d38d8813353ea6
Vishal Pawar <vishal-dev1128@users.noreply.github.com>
2026-04-16 00:51:27 +0530

    Complete redesign: Premium Executive Dashboard with Material 3 branding

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,9 @@
-        </main>
+            </main>
+        </div>
     </div>
+    <script>
+        lucide.createIcons();
+    </script>
     <script src="assets/js/app.js"></script>
 </body>
 </html>
